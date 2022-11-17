eaf9bd2f382b84a973d44aca661701bd5428624e
Dgeko <dfhdfh13@gmail.com>
2022-11-17 20:08:03 +0300

    Подключил шаблонизатор View Engine, использовал динамические шаблоны

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 
 app.listen(PORT, (error) => {
@@ -12,10 +14,31 @@ app.listen(PORT, (error) => {
 });
 
 app.get('/', (req, res) => { // выдает главную страницу
-    res.sendFile(createPath('index'))
+    const title = 'Home';
+    res.render(createPath('index'), {title});
 });
 app.get('/contacts', (req, res) => { // выдает страницу контактов
-    res.sendFile(createPath('contacts'))
+    const title = 'Contacts';
+    const contacts = [
+        {name: 'YouTube', link: 'https://www.youtube.com/watch?v=tlUcmD0zPI4'},
+        {name: 'GitHub', link: 'https://github.com/DgekoTT'},
+    ];
+    res.render(createPath('contacts'), {contacts, title}  );
+});
+
+app.get('/posts/:id', (req, res ) => {
+    const title = 'Post'
+    res.render(createPath( 'posts'), {title});
+});
+
+app.get('/posts', (req, res) => {
+    const title = 'Post'
+    res.render(createPath('posts'), {title});
+});
+
+app.get('/add-post', (req, res) => {
+    const title = 'Add post'
+    res.render(createPath('add-post'), {title});
 });
 
 app.get('/about-us', (req, res) =>{ /* make redirect from one
@@ -25,8 +48,9 @@ app.get('/about-us', (req, res) =>{ /* make redirect from one
 
 app.use((req, res) =>{ /* в случае если пользователь введет 
 неверный адрес, выдаст страницу ошибки и код ошибки */
+    const title = 'Error'
     res
     .status(404)
-    .sendFile(createPath('error'))
+    .sendFile(createPath('error', {title}))
 });
 
