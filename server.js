7f42e58757063899202d6585752e816792f6efb7
Dgeko <dfhdfh13@gmail.com>
2022-11-17 22:43:00 +0300

    вынес head в отдельный файл, добавил шаблоны на страницы, добавил шаблоны в post ans posts, добавил логику по добавлению поста на сайт

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.use(express.static('styles'));// Middleware который разрешает доступ к папке со стилями 
 
+app.use(express.urlencoded({ extend: false}));// парсит входяшие запросы 
+
 app.get('/', (req, res) => { // выдает главную страницу
     const title = 'Home';
     res.render(createPath('index'), {title});
@@ -31,17 +33,44 @@ app.get('/contacts', (req, res) => { // выдает страницу конта
     res.render(createPath('contacts'), {contacts, title}  );
 });
 
+
 app.get('/posts/:id', (req, res ) => {
     const title = 'Post'
-    res.render(createPath( 'posts'), {title});
+    const post = {
+        id: '1',
+        text: 'initial part',
+        title: 'Post title',
+        date: '17.11.22',
+        author: 'Jhohan',
+    };
+    res.render(createPath('post'), {title, post});
 });
 
 app.get('/posts', (req, res) => {
     const title = 'Post'
-    res.render(createPath('posts'), {title});
+    const posts =[ {
+        id: '1',
+        text: 'initial part',
+        title: 'Post title',
+        date: '17.11.22',
+        author: 'Jhohan',
+    }];
+    res.render(createPath('posts'), {title, posts});
 });
 
-app.get('/add-post', (req, res) => {
+app.post('/add-post', (req, res) => { // добавляет пост на сайт
+    const { title, author, body } = req.body;
+    const post = {
+        id: new Date(),
+        data: (new Date().toLocaleDateString()),
+        title,
+        author,
+        body,
+    }
+    res.render(createPath('post'), {post, title});
+});
+
+app.get('/add-post', (req, res) => { // получем страницу добавления на сайт 
     const title = 'Add post'
     res.render(createPath('add-post'), {title});
 });
