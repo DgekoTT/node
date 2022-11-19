5b412f7df175604f4fd0c53fe218639a74b34b98
Dgeko <dfhdfh13@gmail.com>
2022-11-20 01:55:01 +0300

    добавил возможность удаления и редактирования постов

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const morgan = require('morgan')
 const mongoose = require("mongoose");
 const Post = require('./models/post')
-const Contact = require('./models/contacts')
+const Contact = require('./models/contacts');
+const { methods } = require('expres');
+const methodOverride = require('method-override')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -29,6 +31,8 @@ app.use(express.static('styles'));// Middleware который разрешае�
 
 app.use(express.urlencoded({ extend: false}));// парсит входяшие запросы 
 
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => { // выдает главную страницу
     const title = 'Home';
     res.render(createPath('index'), {title});
@@ -44,12 +48,35 @@ app.get('/contacts', (req, res) => { // выдает страницу конта
         });
 });
 
+app.get('/edit/:id', (req, res ) => {
+    const title = 'Edit Post'
+    Post
+        .findById(req.params.id)
+        .then((post) =>  res.render(createPath('edit-post'), {post, title}))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error'})
+        });
+});
 
 app.get('/posts/:id', (req, res ) => {
     const title = 'Post'
     Post
         .findById(req.params.id)
         .then((post) =>  res.render(createPath('post'), {title, post}))
+        .catch((error) => {
+            console.log(error);
+            res.render(createPath('error'), { title: 'Error'})
+        });
+});
+
+
+
+app.delete('/posts/:id', (req, res ) => {
+    const title = 'Post'
+    Post
+        .findByIdAndDelete(req.params.id)
+        .then((result) =>  res.sendStatus(200))
         .catch((error) => {
             console.log(error);
             res.render(createPath('error'), { title: 'Error'})
@@ -90,6 +117,8 @@ app.get('/about-us', (req, res) =>{ /* make redirect from one
  page to another*/
     res.redirect('/contacts')
 });
+
+
 
 app.use((req, res) =>{ /* в случае если пользователь введет 
 неверный адрес, выдаст страницу ошибки и код ошибки */
